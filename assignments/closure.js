ddb4b43fe1933fa9b575f9280528663fdcedd347
Uchechi Nwabia <nwabiaesther@gmail.com>
2022-05-19 18:33:16 +0200

    closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,19 +4,51 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+function food (diet){
+  let rice = 'carbohydrate';
+  let beans = 'protein';
+  
+  function nutrition(foodlover){
+      console.log(`${diet} ${rice} is consumed, you get energy`);
+  }
+return nutrition();
+};
+food('anytime');
+
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
+  let count = 0;
+  return function counter(){
+    count++;
+    if(count > 5){ //is it ojoro putting number 3 solution here? lol
+      return 1
+    }
+    return count;
+    
+  }
   // IMPLEMENTATION OF counterMaker:
   // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
   // 2- Declare a function `counter`. It should increment and return `count`.
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+  
 };
+const myCounter = counterMaker();
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
+
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
 // myCounter(); // 2
@@ -25,9 +57,28 @@ const counterMaker = () => {
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
 
+
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let size = 0;
+ return bigO = 
+     function increment(){
+      size++;
+      return size;
+    };
+    function decrement(){
+      size--;
+      return size;
+    };
+ }
+    
+  
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+
+
+let counterHost = counterFactory();
+console.log(counterHost());
+console.log(counterHost());
+console.log(counterHost());
